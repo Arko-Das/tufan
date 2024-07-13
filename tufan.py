64df7ab843bda6061cbce0cd9c5093622603cf94
Arko-Das <149698669+Arko-Das@users.noreply.github.com>
2024-07-13 18:50:46 +0600

    Create tufan.py
    https://github.com/Arko-Das/tufan.git

---
+++ tufan.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+if first_name == "Tufan" and last_name == "Mondal":
+    print("Welcome Tufan, this tool is made for you.")
+else:
+    name = first_name + ' ' + last_name
+    print('Hello', name)
